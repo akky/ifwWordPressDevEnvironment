e0cafca5be3c92b1cc0489a50fd4c579424c85cb
akky <akimoto@gmail.com>
2014-10-17 16:21:31 +0900

    changed PHPUnit installation method from Pear to Composer.

--- provision/install-phpunit.sh
+++ provision/install-phpunit.sh
@@ -1,6 +1,4 @@
 echo "Installing PHPUnit"
 echo "Hold on, this may take a short while..."
 echo ""
-sudo pear config-set auto_discover 1
-sudo pear upgrade PEAR
-sudo pear install pear.phpunit.de/PHPUnit
+sudo composer global require "phpunit/phpunit=4.3.*"
